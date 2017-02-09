ae04f45923bbe1e0714b3ac80d38ed071a6adee0
meng <xinkuaisuile@126com>
2017-02-10 07:44:25 +0800

    14.1

--- apue/common/include/apue.h
+++ apue/common/include/apue.h
@@ -1,6 +1,13 @@
+/*
+ * Our own header, to be included before all standard system headers.
+ */
+#ifndef _APUE_H
+#define _APUE_H
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXLINE 4096        /* max line length */
 
@@ -11,3 +18,5 @@ void
 clr_fl(int fd, int flags);
 
 void    err_sys(const char *, ...) __attribute__((noreturn));
+
+#endif  /* _APUE_H */
--- apue/common/lib/error.c
+++ apue/common/lib/error.c
@@ -1,3 +1,9 @@
+#include "apue.h"
+#include <errno.h>      /* for definition of errno */
+#include <stdarg.h>     /* ISO C variable arguments */
+
+static void err_doit(int , int , const char *, va_list);
+
 
 /*
  * Fatal error related to a system call.
